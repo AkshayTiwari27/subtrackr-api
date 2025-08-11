514616b9db9fb77c9ef2eeb340820acfa20b5933
Akshay Tiwari <tiwariakshay5902@gmail.com>
2025-08-11 11:43:48 +0530

    Updated nodemailer configuration

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -2,7 +2,7 @@ import nodemailer from 'nodemailer';
 
 import { EMAIL_PASSWORD } from './env.js'
 
-export const accountEmail = 'javascriptmastery00@gmail.com';
+export const accountEmail = 'akshay.tiwari.dev@gmail.com';
 
 const transporter = nodemailer.createTransport({
   service: 'gmail',
